98904640e9c339b6800d36fce5c512f4d9eadb52
이수민 <soomin1601@gmail.com>
2026-06-13 23:35:54 +0900

    feat: 온보딩 닉네임 추가

--- src/features/onboarding/user.ts
+++
@@ -1,6 +0,0 @@
-const FALLBACK_ONBOARDING_USER_NAME = "민경";
-
-export function getOnboardingUserName() {
-	// TODO: replace this fallback with the authenticated user's nickname API.
-	return FALLBACK_ONBOARDING_USER_NAME;
-}
